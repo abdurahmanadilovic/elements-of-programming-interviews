7307716bf873e327d7c6b28fd96ff98bc441e08a
Abdurahman Adilovic <abdurahman.adilovic@gmail.com>
2019-08-14 21:18:18 +0200

    add solution to 17.2

---
+++ hackathon/column1/problem_17_2_levensheiten_distance.py
@@ -0,0 +1,35 @@
+from functools import lru_cache
+
+"""
+operations:
+- insert one char
+- remove one char
+- replace one char
+"""
+
+
+@lru_cache(maxsize=None)
+def dp(source, target):
+    if source == target:
+        return 0
+
+    longest = max(len(source), len(target))
+    total_cost = 0
+
+    for index in range(longest):
+        if index == len(source):
+            return total_cost + len(target) - len(source)
+        if index == len(target):
+            return total_cost + len(source) - len(target)
+
+        if source[index] != target[index]:
+            cost1 = dp(source[index + 1:], target[index + 1:])
+            cost2 = dp(source, target[index + 1:])
+            cost3 = dp(source[index + 1:], target)
+            return 1 + min(cost1, cost2, cost3)
+
+    return total_cost
+
+
+def solution(source, target):
+    return dp(source, target)
---
+++ hackathon/column1/test_17_2.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+from .problem_17_2_levensheiten_distance import solution
+
+
+class TestSolution(TestCase):
+    def testLength1(self):
+        self.assertEqual(1, solution('a', 'b'))
+
+    def testLength2(self):
+        self.assertEqual(2, solution('aa', 'b'))
+
+    def testLength2prefix(self):
+        self.assertEqual(1, solution('ba', 'b'))
+
+    def testLength2suffix(self):
+        self.assertEqual(1, solution('ab', 'b'))
+
+    def testInTheMiddle(self):
+        self.assertEqual(1, solution('aab', 'abb'))
+
+    def testOneEmpty(self):
+        self.assertEqual(3, solution('', 'abb'))
+
+    def testTargetEmpty(self):
+        self.assertEqual(2, solution('aa', ''))
+
+    def testGivenCase(self):
+        self.assertEqual(8, solution('Carthorse', 'Orchestra'))
